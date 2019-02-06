ea89d370f4ce57d4b5a6f3c23c5ac4a62eb754ac
SalmanMKC <32169182+salmanmkc@users.noreply.github.com>
2019-02-06 00:33:12 +0000

    created a method to test generics

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            //instantiate an instance of the Generic class
+            Generic gen = new Generic();
+
+            //test with int, string, bool
+            gen.output<int>(12);
+            gen.output<string>("HELLO");
+            gen.output<bool>(false);
+
+            // prevent auto-close
+            Console.Read();
+        }
+
+        
+    }
+
+    class Generic
+    {
+        public void output<T>(T value)
+        {
+            Console.WriteLine($"{value} is type {typeof(T).Name}");
         }
     }
 }
